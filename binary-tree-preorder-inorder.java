96e63f5498c90469be9af35db49dde0ee86313ab
Aahi Awasthee <aahiawasthee@example.com>
2025-12-02 21:25:35 -0600

    Solving Trees 1 problems

---
+++ binary-tree-preorder-inorder.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int idx; // ptr on preorder
+    HashMap<Integer, Integer> map = new HashMap<>(); //to store inorder values
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        for (int i = 0; i < inorder.length; i++) {
+            this.map.put(inorder[i], i);
+        }
+        return helper(preorder, 0, inorder.length - 1);
+    }
+
+    private TreeNode helper(int[] preorder, int start, int end) {
+        //base
+        if (start > end)
+            return null;
+
+        //logic
+        int rootVal = preorder[idx];
+        idx++;
+        TreeNode root = new TreeNode(rootVal);
+        int rootIdx = map.get(rootVal);
+
+        root.left = helper(preorder, start, rootIdx - 1);
+        root.right = helper(preorder, rootIdx + 1, end);
+
+        return root;
+    }
+}
+
+//TC: O(n) going through the arrays
+//SC: O(n) - Recusrive stack
